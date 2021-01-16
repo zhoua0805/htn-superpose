3303038c88009ef5659c4617d7771009dedd3899
zhoua0805 <alicezhou0805@gmail.com>
2021-01-16 12:22:53 -0500

    attempt to connect django with react

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -18,7 +18,8 @@ from django.urls import path
 from django.views.generic import TemplateView
 
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path(r'^', TemplateView.as_view(template_name="index.html")),
+    path('', TemplateView.as_view(template_name="index.html")),
 ]
